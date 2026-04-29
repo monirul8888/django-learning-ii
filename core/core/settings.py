33cd69d46ff8d4efb303565c9c217e4d715d83d0
monirul8888 <monirul429588@gmail.com>
2026-04-29 22:13:11 +0600

    Added Template Dir

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +59,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
