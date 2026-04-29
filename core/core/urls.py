fec35a4ee349410a7f8b0940792a84fb01a5dadb
monirul8888 <monirul429588@gmail.com>
2026-04-29 21:13:48 +0600

    Added url

--- core/core/urls.py
+++ core/core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from machine.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path ("machine/", machine, name="machine")
 ]
